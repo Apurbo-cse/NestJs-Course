056eb5c2a0f75c5b5921e80f0f544cc8ea398ce6
Apurbo Kumar Anup <apurboka@gmail.com>
2025-06-19 20:04:45 +0600

    Update users.controller.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,20 @@
 import { Controller, Get, Post } from "@nestjs/common";
+import { UsersService } from "./users.service";
 
 @Controller('users')
 export class UsersController {
 
     @Get()
     getUsers() {
-        return 'You made a GET request to all users!'
+        const usersService = new UsersService();
+        return usersService.getAllUsers();
     }
 
     @Post()
-    creatUser(){
+    creatUser() {
+        const user = { id: 3, name: 'Harry', age: 23, gender: 'female', isMarrid: false }
+        const usersService = new UsersService();
+        usersService.createUser(user);
         return 'A new user has been created!'
     }
 }
