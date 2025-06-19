fc71119070b962cab2e8f9708396fa03de6298a6
Apurbo Kumar Anup <apurboka@gmail.com>
2025-06-19 20:45:42 +0600

    Reading query string value

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,17 @@
-import { Controller, Get, Param, Post } from "@nestjs/common";
+import { Controller, Get, Param, Post, Query } from "@nestjs/common";
 import { UsersService } from "./users.service";
 
 @Controller('users')
 export class UsersController {
 
     @Get()
-    getUsers() {
+    getUsers(@Query() query: any) {
         const usersService = new UsersService();
+        if (query.gender) {
+            return usersService.getAllUsers().filter(u => u.gender === query.gender)
+        }
         return usersService.getAllUsers();
     }
-
 
     @Get(':id')
     getUserById(@Param('id') id: any) {
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ export class UsersService {
     users: { id: number; name: string; age: number; gender: string; isMarrid: boolean }[] = [
         { id: 1, name: 'John', age: 28, gender: 'male', isMarrid: false },
         { id: 2, name: 'Mark', age: 32, gender: 'male', isMarrid: true },
+        { id: 2, name: 'Joty', age: 25, gender: 'female', isMarrid: false },
     ];
 
     getAllUsers() {
