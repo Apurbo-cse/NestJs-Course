88e461a5f4b5d111193c9e9417ae44ef95dee0d9
Apurbo Kumar Anup <apurboka@gmail.com>
2025-06-25 01:18:23 +0600

    Connecting to PostgreSQL Database

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,20 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TweetModule } from './tweet/tweet.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, TweetModule, AuthModule],
+  imports: [UsersModule, TweetModule, AuthModule, TypeOrmModule.forRoot({
+    type: 'postgres',
+    entities: [],
+    synchronize: true,
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'admin1234',
+    database: 'nestjs'
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
