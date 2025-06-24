b4e168f7c8066ca03ff254904af833815072dfdb
Apurbo Kumar Anup <apurboka@gmail.com>
2025-06-24 22:55:41 +0600

    Types Of Dependency

--- src/tweet/tweet.controller.ts
+++ src/tweet/tweet.controller.ts
@@ -1,4 +1,8 @@
 import { Controller } from '@nestjs/common';
+import { TweetService } from './tweet.service';
 
 @Controller('tweet')
-export class TweetController {}
+export class TweetController {
+    constructor(private readonly tweetService: TweetService) { }
+}
+
