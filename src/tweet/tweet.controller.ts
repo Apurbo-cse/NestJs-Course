3e98f7f122e6c8d3d58bea57b1e28e11516367da
Apurbo Kumar Anup <apurboka@gmail.com>
2025-06-24 23:35:08 +0600

    Inter Modular Dependency

--- src/tweet/tweet.controller.ts
+++ src/tweet/tweet.controller.ts
@@ -1,8 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { TweetService } from './tweet.service';
 
 @Controller('tweet')
 export class TweetController {
     constructor(private readonly tweetService: TweetService) { }
+
+    @Get(':userId')
+    public GetTweets(@Param('userId', ParseIntPipe) userid: number) {
+        return this.tweetService.getTweets(userid);
+    }
 }
 
