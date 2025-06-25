128bc5c8a6c9769c9bac6619b4c25c427ece7d9a
Apurbo Kumar Anup <apurboka@gmail.com>
2025-06-25 19:40:35 +0600

    Configuring Entity Columns

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsBoolean, IsEmail, isNotEmpty, IsNotEmpty, IsNumber, IsOptional, IsString, MinLength } from "class-validator";
+import { IsBoolean, IsEmail, isNotEmpty, IsNotEmpty, IsNumber, IsOptional, IsString, MaxLength, MinLength } from "class-validator";
 
 export class CreateUserDto {
     @IsNumber()
@@ -7,23 +7,28 @@ export class CreateUserDto {
     @IsString({ message: 'First Name should be a string value.' })
     @IsNotEmpty()
     @MinLength(3, { message: 'First Name should be a minimum of 3 characters' })
+    @MaxLength(100)
     firstName: string;
 
     @IsString({ message: 'Last Name should be a string value.' })
     @IsNotEmpty()
     @MinLength(3, { message: 'Last Name should be a minimum of 3 characters' })
+    @MaxLength(100)
     lastName: string;
 
     @IsEmail()
     @IsNotEmpty()
+    @MaxLength(100)
     email: string;
 
     @IsString()
     @IsOptional()
+    @MaxLength(100)
     gender?: string;
 
     @IsString()
     @IsNotEmpty()
     @MinLength(8)
+    @MaxLength(100)
     password: string
 }
