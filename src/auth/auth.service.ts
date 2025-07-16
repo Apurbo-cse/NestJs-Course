8f320c8b1c32e77a7ed98f62fc8307c19ed02263
Apurbo Kumar Anup <apurboka@gmail.com>
2025-07-16 23:20:00 +0600

    Partial Registration

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,25 @@
 import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
 import { UsersService } from 'src/users/users.service';
+import authConfig from './config/auth.config';
 
 @Injectable()
 export class AuthService {
-    constructor(@Inject(forwardRef(() => UsersService)) private readonly userService: UsersService) { }
+    constructor(
+        @Inject(forwardRef(() => UsersService))
+        private readonly userService: UsersService,
+
+        @Inject(authConfig.KEY)
+        private readonly authConfiguration: ConfigType<typeof authConfig>
+
+    ) { }
 
     isAuthenticketd: Boolean = false
 
     login(email: string, pswd: string) {
-     
+
+        console.log('this.authCofiguration :>> ', this.authConfiguration);
+        console.log('this.authCofiguration :>> ', this.authConfiguration.sharedSecret);
         return 'User does not exist!'
     }
 }
